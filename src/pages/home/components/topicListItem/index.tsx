5c3ccd2851f301c836857293fb470c3afcb8deb4
Kaike <kaikedias98@gmail.com>
2023-09-28 23:12:47 -0300

    refactor: Refactored CSS for Improved Organization

--- src/pages/home/components/topicListItem/index.tsx
+++ src/pages/home/components/topicListItem/index.tsx
@@ -1,5 +1,5 @@
-import { format } from 'date-fns';
-import './styles.css';
+import { format } from "date-fns";
+import "./styles.css";
 
 interface TopicListItemProps {
   topic: Topic;
@@ -7,16 +7,23 @@ interface TopicListItemProps {
   onDislike: (topicId: string) => void;
 }
 
-export function TopicListItem({ topic, onLike, onDislike }: TopicListItemProps) {
+export function TopicListItem({
+  topic,
+  onLike,
+  onDislike,
+}: TopicListItemProps) {
   // Formatar a data para exibir apenas o dia da semana
-  const formattedDate = format(topic.created_at, 'EEEE');
+  const formattedDate = format(topic.created_at, "EEEE");
 
   const balance = topic.upVote - topic.downVote;
 
+  const balanceClass =
+    balance > 0 ? "positive-balance" : balance < 0 ? "negative-balance" : "";
+
   return (
     <div>
-      <li className='card'>
-        <div className='header'>
+      <li className="card">
+        <div className="header">
           <div className="tag-list">
             {topic.tags.map((tag, index) => (
               <span key={index} className="tag">
@@ -24,33 +31,47 @@ export function TopicListItem({ topic, onLike, onDislike }: TopicListItemProps) 
               </span>
             ))}
           </div>
-          <div id='date'><p>{formattedDate}</p></div>
-        </div>
-        <div className='main-content'>
-          <p className='description-text'>{topic.description}</p>
-        </div>
-        <div className='footer'>
-          <div className='action-bar'>
-              <div className='like-button'>
-                <button onClick={() => onLike(topic.id)}>
-                  <span role="img" aria-label="Thumbs up">👍</span>
-                </button>
-              </div>
-              <div className='dislike-button'>
-                <button onClick={() => onDislike(topic.id)}>
-                  <span role="img" aria-label="Thumbs down">👎</span>
-                </button>
-              </div>
-            <div className='balance'>
-              <p>{balance}</p>
-            </div>
+          <div id="date">
+            <p>{formattedDate}</p>
           </div>
-          <div className='author-info'>
-            <p id='author-name'>{topic.author.name}</p>
+        </div>
+        <div className="main-content">
+          <p className="description-text">{topic.description}</p>
+        </div>
+        <div className="footer">
+          <div className="votes-area">
+            <div className="action-bar">
+              <div className="like-button">
+                <button onClick={() => onLike(topic.id)}>
+                  <span role="img" aria-label="Thumbs up">
+                    👍
+                  </span>
+                </button>
+              </div>
+              <div className="dislike-button">
+                <button onClick={() => onDislike(topic.id)}>
+                  <span role="img" aria-label="Thumbs down">
+                    👎
+                  </span>
+                </button>
+              </div>
+              <div className={`balance ${balanceClass}`}>
+                <p>{balance}</p>
+              </div>
+            </div>
+            <progress
+              className="progress"
+              value={topic.upVote}
+              max={topic.upVote + topic.downVote}
+            ></progress>
+          </div>
+
+          <div className="author-info">
+            <p id="author-name">{topic.author.name}</p>
             <p>{topic.author.city}</p>
           </div>
         </div>
-    </li>
+      </li>
     </div>
   );
 }
